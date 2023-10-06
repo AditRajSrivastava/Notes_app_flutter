d08842ae59c3e06a84bd73a8768594030d224d91
AditRajSrivastava <adityasrivastava13112003@gmail.com>
2023-10-06 12:11:42 +0530

    step-6

--- lib/views/login_view.dart
+++ lib/views/login_view.dart
@@ -65,9 +65,20 @@ class _LoginViewState extends State<LoginView> {
                     .pushNamedAndRemoveUntil(notesRoute, (route) => false);
               } on FirebaseAuthException catch (e) {
                 if (e.code == "user-not-found") {
-                  devtools.log("user-not-found");
+                  await showErrorDialog(
+                    context,
+                    "User not found",
+                  );
                 } else if (e.code == 'wrong-password') {
-                  devtools.log("wrong-password");
+                  await showErrorDialog(
+                    context,
+                    "Wrong password",
+                  );
+                } else {
+                  await showErrorDialog(
+                    context,
+                    'Error: ${e.code}',
+                  );
                 }
               }
             },
@@ -86,4 +97,26 @@ class _LoginViewState extends State<LoginView> {
       ),
     );
   }
+}
+
+Future<void> showErrorDialog(
+  BuildContext context,
+  String text,
+) {
+  return showDialog(
+    context: context,
+    builder: (context) {
+      return AlertDialog(
+        title: const Text("An error occured"),
+        content: Text(text),
+        actions: [
+          TextButton(
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+              child: const Text("OK"))
+        ],
+      );
+    },
+  );
 }
